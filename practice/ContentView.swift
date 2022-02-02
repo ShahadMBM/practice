83464819fb7d8c3bed4ce3087cb67ff2eed3c16b
Shahad Benmohi <96819901+ShahadMBM@users.noreply.github.com>
2022-02-02 10:24:33 +0300

    Initial Commit

---
+++ practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  practice
+//
+//  Created by Shahad on 01/07/1443 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ practice/practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  practiceApp.swift
+//  practice
+//
+//  Created by Shahad on 01/07/1443 AH.
+//
+
+import SwiftUI
+
+@main
+struct practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
